770b40acfa6f4cc52527a4977d5dc8e58938da4c
veronika <wawonika@gmail.com>
2025-11-24 06:49:17 +0000

    updated the databases, fixed syntax errors

--- myPWA/.database/myQuery.sql
+++ myPWA/.database/myQuery.sql
@@ -1,21 +1,21 @@
-CREATE TABLE site_objects(
-    index = INT PRIMARY KEY
-    image = VARCHAR(50)
-    name = VARCHAR(50)
-    year = INT
-    brand = VARCHAR(50)
-    model_num = VARCHAR(50)
-    type = VARCHAR(50)
-    colour = VARCHAR(50)
-    description = VARCHAR(250)
-    );
+CREATE TABLE site_objects (
+    obj_index INTEGER PRIMARY KEY,
+    image VARCHAR(50),
+    device_name VARCHAR(50),
+    year INT,
+    brand VARCHAR(50),
+    model_num VARCHAR(50),
+    type VARCHAR(50),
+    colour VARCHAR(50),
+    description VARCHAR(150)
+);
 
 CREATE TABLE suggestions(
-    name = VARCHAR(50)
-    email = VARCHAR(50)
-    suggestion = VARCHAR(250)
+    name VARCHAR(50),
+    email VARCHAR(50),
+    suggestion VARCHAR(250)
     );
 
-INSERT INTO site_objects(index,image,name,year,brand,model_num,type,colour,description) VALUES (1,"imacg3.png","iMac G3", 1998, 000, "computer", "blue", "sample description");
+INSERT INTO site_objects(obj_index,image,device_name,year,brand,model_num,type,colour,description) VALUES (1,'/public/images/imacg3.png','iMac G3', 1998, 'Apple', 000, 'computer', 'blue', 'sample description');
 
 SELECT * FROM site_objects;
